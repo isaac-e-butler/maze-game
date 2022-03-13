0072b2c3c4f6f9eca7bef0f29c8285f007d0df13
Isaac <gr25.main@gmail.com>
2022-03-13 00:16:43 +0000

    changing to node.js

--- src/components/maze.jsx
+++
@@ -1,9 +0,0 @@
-import './maze.css';
-import React from 'react';
-import Grid from './display/grid';
-
-function Maze() {
-    return <Grid maxCol={30} maxRow={20} char={'@'} />;
-}
-
-export default Maze;
